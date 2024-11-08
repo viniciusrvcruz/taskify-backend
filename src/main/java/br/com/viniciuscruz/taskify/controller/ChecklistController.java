20f7338034bf45d32ac0b06c41a010f9f1564210
gobi <viniciusrobertopro@gmail.com>
2024-11-08 11:29:11 -0300

    feat: documentation with swagger

--- src/main/java/br/com/viniciuscruz/taskify/controller/ChecklistController.java
+++ src/main/java/br/com/viniciuscruz/taskify/controller/ChecklistController.java
@@ -1,9 +1,16 @@
 package br.com.viniciuscruz.taskify.controller;
 
 import br.com.viniciuscruz.taskify.dto.ChecklistDto;
+import br.com.viniciuscruz.taskify.exception.CustomExceptionResponse;
 import br.com.viniciuscruz.taskify.service.ChecklistService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,36 +18,81 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/checklists")
+@Tag(name = "Checklists", description = "Here are the endpoints to manage checklists")
 public class ChecklistController {
 
     @Autowired
     private ChecklistService service;
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Persist a new checklist in database.", tags = {"Checklists"}, responses =
+            { @ApiResponse(description = "Success", responseCode = "200", content = { @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ChecklistDto.class)
+            )}),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = { @Content }),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = { @Content }),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = { @Content })
+            }
+    )
     public ResponseEntity<ChecklistDto> create(@RequestBody ChecklistDto checklistDto) {
         ChecklistDto checklist = service.create(checklistDto);
         return new ResponseEntity<>(checklist, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", consumes = MediaType.ALL_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Find a checklist by id", tags = {"Checklists"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ChecklistDto.class))
+            }),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = { @Content }),
+            @ApiResponse(description = "Resource Not Found", responseCode = "404", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema( implementation = CustomExceptionResponse.class))
+            }),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = { @Content })
+    })
     public ResponseEntity<ChecklistDto> findById(@PathVariable(name = "id") long id) {
         ChecklistDto checklist = service.findById(id);
         return new ResponseEntity<>(checklist, HttpStatus.OK);
     }
 
-    @PutMapping
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Update an existing checklist.", tags = {"Checklists"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ChecklistDto.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<ChecklistDto> update(@RequestBody ChecklistDto checklistDto) {
         ChecklistDto checklist = service.update(checklistDto);
         return new ResponseEntity<>(checklist, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a checklist by ID.", tags = {"Checklists"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Resource Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity delete(@PathVariable(name = "id") long id) {
         service.delete(id);
         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Retrieve all checklists.", tags = {"Checklists"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ChecklistDto.class))),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<List<ChecklistDto>> findAll(){
         var checklists = service.findAll();
         return new ResponseEntity<>(checklists, HttpStatus.OK);
--- src/main/java/br/com/viniciuscruz/taskify/controller/TaskController.java
+++ src/main/java/br/com/viniciuscruz/taskify/controller/TaskController.java
@@ -2,6 +2,11 @@ package br.com.viniciuscruz.taskify.controller;
 
 import br.com.viniciuscruz.taskify.dto.TaskDto;
 import br.com.viniciuscruz.taskify.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,40 +16,86 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
+@Tag(name = "Tasks", description = "Endpoints for managing tasks")
 public class TaskController {
 
     @Autowired
     private TaskService service;
 
     @PostMapping
+    @Operation(summary = "Create a new task.", tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Task created successfully", responseCode = "201",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskDto.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content)
+            }
+    )
     public ResponseEntity<TaskDto> create(@RequestBody TaskDto taskDto) {
         TaskDto model = service.create(taskDto);
         return new ResponseEntity<>(model, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Retrieve a task by ID.", tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Task retrieved successfully", responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskDto.class))),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content)
+            }
+    )
     public ResponseEntity<TaskDto> findById(@PathVariable(name = "id") long id) {
         TaskDto task = service.findById(id);
         return new ResponseEntity<>(task, HttpStatus.OK);
     }
 
     @PutMapping
+    @Operation(summary = "Update an existing task.", tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Task updated successfully", responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskDto.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content)
+            }
+    )
     public ResponseEntity<TaskDto> update(@RequestBody TaskDto taskDto) {
         TaskDto task = service.update(taskDto);
         return new ResponseEntity<>(task, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a task by ID.", tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Task deleted successfully", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content)
+            }
+    )
     public ResponseEntity delete(@PathVariable(name = "id") long id) {
         service.delete(id);
         return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
     }
 
     @GetMapping
+    @Operation(summary = "Retrieve all tasks.", tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Tasks retrieved successfully", responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskDto.class))),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<List<TaskDto>> findAll(){
         var list = service.findAll();
         return new ResponseEntity<List<TaskDto>>(list, HttpStatus.OK);
     }
 
-    // TODO: findByTitle
+    @GetMapping("/find/title/{title}")
+    @Operation(summary = "Find tasks by title.", tags = {"Tasks"},
+            responses = {
+                    @ApiResponse(description = "Tasks found successfully", responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = TaskDto.class))),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content)
+            }
+    )
+    public ResponseEntity<List<TaskDto>> findByTitle(@PathVariable(name = "title") String title) {
+        var tasks = service.findByTitle(title);
+        return new ResponseEntity<List<TaskDto>>(tasks, HttpStatus.OK);
+    }
 }
